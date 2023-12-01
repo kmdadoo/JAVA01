d6603ea6513e4acb921c4043bd695e11b2e3bcb7
DESKTOP-1KNPDRV\javastudy <kmdadoo1@gmail.com>
2023-12-01 15:27:19 +0900

    ~최종

--- src/ex21jdbc/prepared/InsertSQL.java
+++ src/ex21jdbc/prepared/InsertSQL.java
@@ -1,5 +1,6 @@
 package ex21jdbc.prepared;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import ex21jdbc.connect.IConnectImpl;
@@ -47,15 +48,21 @@ public class InsertSQL extends IConnectImpl
 			psmt.setString(2, pw);
 			psmt.setString(3, name);
 			
-
-			psmt.setString(4, "2022-10-27");
+			// 날짜입력1 : 날짜를 문자열로 입력하는 경우
+//			psmt.setString(4, "2022-10-27");
 			
-	
-//			java.util.Date utilDate = new java.util.Date();
-//			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-//			psmt.setDate(4, sqlDate);			
-			 
-
+			// 날자 입력2 : Date형으로 입력하는 경우
+			/*
+			현재날짜를 Java에서 입력하는 경우 아래와 같이 변환과정을
+			거쳐야 한다.  Util패키지로 시간을 얻어온 후 sql패키지로 타입을
+			변환한다. 이때는 date형으로 입력되므로 setDate()로 인파라미터를
+			설정해야 한다.
+			*/
+			Date utilDate = new Date();
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+			psmt.setDate(4, sqlDate);
+			
+			// 5. 쿼리실행 및 결과값 반환
 			int affected = psmt.executeUpdate();
 			System.out.println(affected +"행이 입력되었습니다.");
 		}
